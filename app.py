3998d8750ec0aff3c9d7104e037d1f467671abfe
Py <42725991+yiwanggecaonima@users.noreply.github.com>
2019-08-15 22:02:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 # coding=utf-8
 from flask import Flask
-
+from redis import Redis
 
 import os
+redis_client = Redis(host="localhost",port=6379)
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	
-	return "Hello World! i have been seen 不知道 times."
+	return "Hello World!"
 
 app.run(host="0.0.0.0",debug=True)
